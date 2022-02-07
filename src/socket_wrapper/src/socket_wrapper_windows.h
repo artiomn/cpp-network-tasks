308e29baaa4505a6f60d11beab2f320240eae160
artiom <artiom@nomail.cpm>
2022-02-07 19:43:50 +0300

    Fix.

--- src/socket_wrapper/src/socket_wrapper_windows.h
+++ src/socket_wrapper/src/socket_wrapper_windows.h
@@ -46,7 +46,7 @@ public:
     {
         // return std::strerror(std::errno);
         char *s = NULL;
-        FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+        FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
             NULL, get_last_error_code(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&s, 0, NULL);
         std::string result{s};
         LocalFree(s);
