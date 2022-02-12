aa1b50c4229bda3409da00e7ae8e03e65677f8a5
Artiom N <artiom@artiom>
2022-02-13 01:22:55 +0300

    Blank lines were added

--- src/l2/raw_sniffer/pcap_structures.h
+++ src/l2/raw_sniffer/pcap_structures.h
@@ -2,17 +2,20 @@
 
 #include <cstdint>
 
+
 // Copied from libpcap to synthesize a PCAP file.
 const auto PCAP_VERSION_MAJOR = 2;
 const auto PCAP_VERSION_MINOR = 4;
 const auto DLT_EN10MB = 1;
 const auto BUFFER_SIZE_PKT = 256 * 256 - 1;
 
+
 struct pcap_timeval
 {
 	int32_t tv_sec;
 	int32_t tv_usec;
 };
+
 
 struct pcap_file_header
 {
@@ -25,6 +28,7 @@ struct pcap_file_header
 	uint32_t linktype = DLT_EN10MB;
 };
 
+
 struct pcap_sf_pkthdr
 {
 	struct pcap_timeval ts;
@@ -32,12 +36,14 @@ struct pcap_sf_pkthdr
 	uint32_t len;
 };
 
+
 // Various sizes and offsets for our packet read buffer.
 const auto BUFFER_SIZE_HDR = sizeof(pcap_sf_pkthdr);
 const auto BUFFER_SIZE_ETH = 14;
 const auto BUFFER_SIZE_IP = BUFFER_SIZE_PKT - BUFFER_SIZE_ETH;
 const auto BUFFER_OFFSET_ETH = sizeof(pcap_sf_pkthdr);
 const auto BUFFER_OFFSET_IP = BUFFER_OFFSET_ETH + BUFFER_SIZE_ETH;
+
 
 #if defined(WIN32)
 const auto BUFFER_WRITE_OFFSET = BUFFER_OFFSET_IP;
