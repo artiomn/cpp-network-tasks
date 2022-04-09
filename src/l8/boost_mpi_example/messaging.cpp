d489e9515ab7c2c90a12327a956c01256490af3f
Artiom N <artiom@artiom>
2022-04-09 12:16:20 +0300

    Boost.MPI messaging example was added

---
+++ src/l8/boost_mpi_example/messaging.cpp
@@ -0,0 +1,35 @@
+#include <boost/mpi.hpp>
+#include <iostream>
+#include <string>
+#include <boost/serialization/string.hpp>
+
+
+namespace mpi = boost::mpi;
+
+
+int main()
+{
+  mpi::environment env;
+  mpi::communicator world;
+
+    if (0 == world.rank())
+    {
+        world.send(1, 0, std::string("Hello"));
+        std::string msg;
+        world.recv(1, 1, msg);
+        std::cout
+            << msg << "!"
+            << std::endl;
+    }
+    else
+    {
+        std::string msg;
+        world.recv(0, 0, msg);
+        std::cout
+            << msg << ", ";
+        std::cout.flush();
+        world.send(0, 1, std::string("world"));
+    }
+
+    return EXIT_SUCCESS;
+}
