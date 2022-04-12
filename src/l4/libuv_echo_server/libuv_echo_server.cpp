1012ffe8a27ed9ed50baa36778976d73cf7b0cd0
Artiom N <artiom@artiom>
2022-04-12 13:24:36 +0300

    Libuv echo server example was fixed

--- src/l4/libuv_echo_server/libuv_echo_server.cpp
+++ src/l4/libuv_echo_server/libuv_echo_server.cpp
@@ -16,7 +16,7 @@ void alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf)
 {
     (void)handle;
 
-    buf->base = (char*)malloc(suggested_size);
+    buf->base = new char[suggested_size];
     buf->len = suggested_size;
 }
 
@@ -25,7 +25,9 @@ void echo_write(uv_write_t *req, int status)
 {
     if (status)
     {
-        std::cerr <<  "Write error " <<  uv_strerror(status) << std::endl;
+        std::cerr
+            << "Write error " <<  uv_strerror(status)
+            << std::endl;
     }
 
     delete req;
@@ -38,7 +40,9 @@ void echo_read(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf)
     {
         if (nread != UV_EOF)
         {
-            std::cerr << "Read error " <<  uv_err_name(nread) << std::endl;
+            std::cerr
+                << "Read error " <<  uv_err_name(nread)
+                << std::endl;
             uv_close(reinterpret_cast<uv_handle_t*>(client), nullptr);
         }
     }
@@ -51,7 +55,7 @@ void echo_read(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf)
 
     if (buf->base)
     {
-        free(buf->base);
+        delete[] buf->base;
     }
 }
 
@@ -60,26 +64,29 @@ void on_new_connection(uv_stream_t *server, int status)
 {
     if (status < 0)
     {
-        std::cerr << "New connection error " << uv_strerror(status) << std::endl;
+        std::cerr
+            << "New connection error "
+            << uv_strerror(status) << std::endl;
         return;
     }
 
-    uv_tcp_t *client = (uv_tcp_t*)malloc(sizeof(uv_tcp_t));
+    auto client = new uv_tcp_t;
     uv_tcp_init(loop, client);
-    if (0 == uv_accept(server, (uv_stream_t*) client))
+
+    if (0 == uv_accept(server, reinterpret_cast<uv_stream_t*>(client)))
     {
-        uv_read_start((uv_stream_t*)client, alloc_buffer, echo_read);
+        uv_read_start(reinterpret_cast<uv_stream_t*>(client), alloc_buffer, echo_read);
     }
     else
     {
-        uv_close((uv_handle_t*) client, NULL);
+        uv_close(reinterpret_cast<uv_handle_t*>(client), nullptr);
     }
 }
 
 
 int main()
 {
-	unsigned short port = 8192;
+    unsigned short port = 8192;
 
     loop = uv_default_loop();
 
@@ -89,7 +96,7 @@ int main()
     uv_ip4_addr("0.0.0.0", port, &addr);
 
     uv_tcp_bind(&server, reinterpret_cast<const struct sockaddr*>(&addr), 0);
-    int r = uv_listen((uv_stream_t*)&server, 128, on_new_connection);
+    int r = uv_listen(reinterpret_cast<uv_stream_t*>(&server), 128, on_new_connection);
     if (r)
     {
         std::cerr << "Listen error " << uv_strerror(r) << std::endl;
